b694b90b4f59e9ef7b6bb17dc27c852252884343
Mark Essen <essen93@bk.ru>
2022-12-17 20:11:16 +0400

    Создал класс User

---
+++ Buisness/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buisness.Models
+{
+    public enum Role
+    {
+        User,
+        Admin
+    }
+
+    public class User
+    {
+        public readonly int Id;
+        public string Name;
+        public string Username;
+        public string Password;
+        public Role Role;
+
+
+
+        public void RequestSupport()
+        {
+            Console.WriteLine("Ожидайте звонка.");
+        }
+    }
+}
